fa498a545981a0257e74e3f3ba45f603df6ad3c5
Professional <sssemkiqqq@mail.ru>
2022-08-21 07:45:36 +0300

    commit my homework 5 :))

--- lesson5_1.py
+++ lesson5_1.py
@@ -1,6 +1,6 @@
 #1
 with open("text.txt", "w", encoding="utf-8") as f_obj:
-    s = input()
+    s = input("введите данные, после ввода пустой строки - ввод завершен ")
     while s != "":
         f_obj.writelines(f"введено через Python - {s}\n")
-        s = input()
+        s = input("введите данные, после ввода пустой строки - ввод завершен ")
